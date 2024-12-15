34c3a1aab5373f919ba12c41e61e036df735de4d
Aditya Raj <aditya040mth@gmail.com>
2024-12-16 01:08:52 +0530

    Project Basic Setup

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './Header.css'
+
+export default function Header({}) {
+    const [data, setData] = useState(false);
+    useEffect(() => {
+        console.log("hello ")
+        header.current.style.background = "red";
+
+    }, [data])
+    const header = useRef(null);
+    return (
+        <div className='Header' ref={header}>
+            This is my header for {data}
+
+        </div>
+    )
+}
---
+++ server/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+import dotenv from 'dotenv'; 
+
+dotenv.config();
+const app = express(); 
+const PORT = 
+app.listen(process.env.PORT)
