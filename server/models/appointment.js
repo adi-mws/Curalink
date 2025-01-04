048f3087e99bdefbe90dc27ccdf20e36a0dc8342
Aditya Raj <aditya040mth@gmail.com>
2025-01-04 22:00:09 +0530

    Icons added eye and eye-slash

---
+++ server/models/appointment.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const AppointmentSchema = new mongoose.Schema({
+  appointmentDate: { type: Date, required: true },
+  appointmentTime: { type: String, required: true },
+  duration: { type: Number, required: true }, // in minutes
+  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
+  doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
+  service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
+  followUpDuration: { type: Number, required: false }, // in days (if applicable)
+  status: { type: String, enum: ['Scheduled', 'Completed', 'Cancelled'], default: 'Scheduled' },
+  treatment: {
+    doctorName: { type: String },
+    prescription: [
+      {
+        medicine: { type: String, required: true },
+        dosage: { type: String },
+        frequency: { type: String, required: true },
+        duration: { type: String, required: true },
+        instructions: { type: String, required: true }
+      }
+    ],
+    diagnosis: { type: String, required: true },
+    treatment: { type: String, required: true },
+    reports: [
+      {
+        fileUrl: { type: String, required: true },
+        description: { type: String }
+      }
+    ],
+    recommendedTests: [
+      {
+        testName: { type: String, required: true },
+        description: { type: String },
+        instructions: { type: String }
+      }
+    ],
+    notes: { type: String },
+    createdByPatient: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+const Appointment = mongoose.model('Appointment', AppointmentSchema);
+
+module.exports = Appointment;
