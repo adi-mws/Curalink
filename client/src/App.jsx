d11e2cc6ed9a35747fe6dd5fd704db3d95311a57
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-01-06 17:58:18 +0530

    Footer creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage.jsx'
 import CompleteYourProfile from './components/Primitive/CompleteYourProfile/CompleteYourProfile.jsx'
+import Footer from './components/Footer_Section/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
       {/* <HomePage />  */}
-      <CompleteYourProfile />  
+      {/* <CompleteYourProfile />   */}
+      < Footer/>
     </>
   )
 }
