bb4f4c16c0bb1ac24cfb60b3003093bb214b8db9
Aditya Raj <aditya040mth@gmail.com>
2025-01-06 20:08:32 +0530

    Fixed CSS of Footer and Complete Your Profile | Created Responsive Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage.jsx'
 import CompleteYourProfile from './components/Primitive/CompleteYourProfile/CompleteYourProfile.jsx'
-import Footer from './components/Footer_Section/Footer.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +11,7 @@ function App() {
     <>
       {/* <HomePage />  */}
       {/* <CompleteYourProfile />   */}
-      < Footer/>
+      {/* < Footer/> */}
     </>
   )
 }
