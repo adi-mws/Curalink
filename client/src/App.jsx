f87e70a79f4a43eeca4fa5c7bc7a1d704f64546e
Aditya Raj <aditya040mth@gmail.com>
2025-01-07 20:38:10 +0530

    App.jsx is empty now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import PatientLoginForm from './components/forms/PatientLoginForm/PatientLoginForm'
 function App() {
   return (
     <>
-      <PatientLoginForm />
     </>
   )
 }
