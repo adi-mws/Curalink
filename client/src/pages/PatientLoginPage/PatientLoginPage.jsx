c9888cf4dd89e7c92efac45d944ddf4bf480bcf9
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-01-10 20:10:43 +0530

    Creating PatientLoginPage

---
+++ client/src/pages/PatientLoginPage/PatientLoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./PatientLoginPage.css";
+
+const PatientLoginPage = () => {
+  return (
+    <div className='PatientLoginPage'>
+
+    </div>
+  )
+}
+
+export default PatientLoginPage
