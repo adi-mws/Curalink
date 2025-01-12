44cfd8f15263715a3d889269958f8286afe467ea
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-01-12 23:27:36 +0530

    patient registration form started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/layout/Header/Header'
-import PatientLoginForm from './components/forms/PatientLoginForm/PatientLoginForm.jsx'
 function App() {
   return (
     <>
-    <PatientLoginForm/>
     </>
   )
 }
