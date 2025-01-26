70d97faea3cec762d18f2372f0ecc36999c8b910
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-01-26 22:39:42 +0530

    FAQs component fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import ContactPage from './pages/ContactPage/ContactPage.jsx'
 import Header from './components/layout/Header/Header.jsx'
 import Footer from './components/layout/Footer/Footer.jsx'
+import FAQs from './components/FAQs/FAQs.jsx'
 
 
 function App() {
   return (
     <>
-    <Header/>
-    <ContactPage />
-    <Footer/>
+    <FAQs></FAQs>
     </>
   )
 }
