590a4e73f3b78be4ccf134a13b4f85707b7662a7
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-02-03 23:25:21 +0530

    Doctors Page Hero Section Done!

--- client/src/pages/DoctorsPage/DoctorsPage.jsx
+++ client/src/pages/DoctorsPage/DoctorsPage.jsx
@@ -1,16 +1,34 @@
 import './DoctorsPage.css'
 import React from 'react'
+import HeroSectionSlope from '../../assets/imgs/heroDoctorsPage-slope.png'
+import MainHeroImage from '../../assets/imgs/heroDoctorPage.png'
 import PrimaryButton from '../../components/shared/buttons/PrimaryButton/PrimaryButton.jsx'
 
 export default function DoctorsPage() {
   return (
     <div className="DoctorsPage">
-        <div style={{backgroundImage: `url(${HeroBackgroundSlope})` }} className="Hero-Section">
+        <div style={{backgroundImage: `url(${HeroSectionSlope})`,backgroundSize:"cover" }} className="Hero-Section">
             <div className="Hero-content">
                 <p className="Heading">Join Us in Revolutionizing Healthcare</p>
                 <p className="Body">Become a part of our mission to provide exceptional care through innovation, compassion, and expertise. Together, let's transform lives and shape the future of online healthcare.</p>
-                <PrimaryButton text="Join Us"/>
-                
+                <PrimaryButton text="Join Us" width='12em'/>
+            </div>
+            <div className="MainImage">
+                <img src={MainHeroImage} alt="" />
+            </div>
+            <div className="Hero-Merits">
+              <div className="Merit-1">
+                <p className="Merit-Head">80+</p>
+                <p className="Merit-Body">Specialists</p>
+              </div>
+              <div className="Merit-2">
+                <p className="Merit-Head">27/7</p>
+                <p className="Merit-Body">Support</p>
+              </div>
+              <div className="Merit-3">
+                <p className="Merit-Head">10%</p>
+                <p className="Merit-Body">Platform Charge</p>
+              </div>
             </div>
         </div>
 
