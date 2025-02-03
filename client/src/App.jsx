27fd6955db39d601c1729a05c32339a0b4e9c30c
Vedant771 <vedantgupta771@gmail.com>
2025-02-03 17:05:56 +0530

    Almost Completed The About Page!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import NewsPage from './pages/NewsPage/NewsPage'
 import Pagination from './components/shared/Pagination/Pagination'
+import AboutPage from './pages/AboutPage/AboutPage'
+import Header from './components/layout/Header/Header'
+import Footer from './components/layout/Footer/Footer'
 
 
 function App() {
   return (
-    <NewsPage/>
+    <>
+    <Header />
+    <AboutPage />
+    <Footer />
+    </>
   )
 }
 export default App
