71b187982bf492e22bc0b3bada555eb69dde444c
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-02-04 22:04:53 +0530

    Support Page Test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import NewsPage from './pages/NewsPage/NewsPage'
 import DoctorsPage from './pages/DoctorsPage/DoctorsPage.jsx'
 import Pagination from './components/shared/Pagination/Pagination'
+import SupportPage from './pages/SupportPage/SupportPage.jsx'
 import AboutPage from './pages/AboutPage/AboutPage.jsx'
 
 
 function App() {
   return (
-    <DoctorsPage/>
+    <SupportPage/>
   )
 }
 export default App
