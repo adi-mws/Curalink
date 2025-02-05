46c10e765e30d873c837603335faeea38ef7d1a2
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-02-05 20:37:23 +0530

    Checking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,13 @@ import './App.css'
 import NewsPage from './pages/NewsPage/NewsPage'
 import NewsSearchPage from './pages/NewsSearchPage/NewsSearchPage.jsx'
 import DoctorsPage from './pages/DoctorsPage/DoctorsPage.jsx'
-import Pagination from './components/shared/Pagination/Pagination'
 import SupportPage from './pages/SupportPage/SupportPage.jsx'
 import AboutPage from './pages/AboutPage/AboutPage.jsx'
 
 
 function App() {
   return (
-    <NewsSearchPage/>
+    <DoctorsPage/>
   )
 }
 export default App
