3e28dd5b07bbb4296ed27bef36fb70e6cad45f83
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-02-05 17:36:22 +0530

    CreatedInfoHeader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,12 @@ import DoctorsPage from './pages/DoctorsPage/DoctorsPage.jsx'
 import Pagination from './components/shared/Pagination/Pagination'
 import SupportPage from './pages/SupportPage/SupportPage.jsx'
 import AboutPage from './pages/AboutPage/AboutPage.jsx'
+import UserInfoHeader from './components/layout/UserInfoHeader/UserInfoHeader.jsx'
 
 
 function App() {
   return (
-    <NewsSearchPage/>
+    <UserInfoHeader />
   )
 }
 export default App
