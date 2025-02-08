fd36c547bc23d2f344126e739ebc3cbc1c5ba46d
Vedant771 <vedantgupta771@gmail.com>
2025-02-08 23:25:34 +0530

    Hey There!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import NewsPage from './pages/NewsPage/NewsPage'
 import Pagination from './components/shared/Pagination/Pagination'
+import Header from './components/layout/Header/Header'
+import AboutPage from './pages/AboutPage/AboutPage'
+import Footer from './components/layout/Footer/Footer'
+
 
 
 function App() {
   return (
-    <NewsPage/>
+    <>
+      <Header />
+      <AboutPage />
+      <Footer />
+    </>
   )
 }
 export default App
