6b5c8cc280d85c00eac660291822d364dd919b82
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-02-14 21:55:31 +0530

    News Page Responsiveness fixed

---
+++ client/src/pages/DoctorProfilePage/DoctorPublicProfilePage.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './DoctorPublicProfilePage.css'
+import RatingDisplay from '../../components/Rating/RatingDisplay.jsx'
+import DummyImage from '../../assets/imgs/Dummy-Doctor-Profile-Picture.png'
+import MenuIcon from '../../assets/icons/Menu-Book.png'
+import RefreshIcon from '../../assets/icons/refresh-2.png'
+import CopyIcon from '../../assets/icons/copy-icon.png'
+
+export default function DoctorPublicProfilePage() {
+    const doctor = {
+        name: "Dr. Aisha Sharma",
+        category: "Cardiologist",
+        profilePicture: DummyImage,
+        rating: 4.8,
+        consultations: 1200,
+        totalReviews: 300,
+        about: "Dr. Aisha Sharma is a renowned cardiologist with over 15 years of experience in treating heart-related conditions. She specializes in non-invasive cardiology and preventive care.",
+        licenceNo: "ahskdfh432"
+    }
+  return (
+    <div className='DoctorPublicProfilePage'>
+        <div className="Doctor-Profile">
+            <div className="Profile-Picture-Section">
+                <div className="Profile-Picture">
+                    <img src={doctor.profilePicture} alt="" />
+                </div>
+            </div>
+            <div className="Doctor-Details">
+                <p className="Doctor-Name">{doctor.name}</p>
+                <p className="Doctor-Category">{doctor.category}</p>
+                <div className="Doctor-Rating"><p style={{fontSize:'2rem',fontWeight:'500'}}>{doctor.rating}</p>
+                <RatingDisplay rating={doctor.rating} /></div>
+                <div className="Consultation-Reviews">
+                    <div className="Consultation">
+                        <p>{doctor.consultations} Consultations</p>
+                    </div>
+                    <div className="Reviews">
+                        <p>{doctor.totalReviews} Reviews</p>
+                    </div>
+                </div>   
+            </div>
+
+            <div className="Buttons">
+                <button className="Book-Appointment"><img src={MenuIcon} alt="" /></button>
+                <button className="Prescription-Renewal"><img src={RefreshIcon} alt="" /></button>
+            </div>
+            <div className="About-Doctor">
+                    <p className='About-Doctor-Title'>About Doctor</p>
+                    <p className='About-Doctor-Description'>{doctor.about}</p>
+            </div>
+            <p className="Licence-Id">Licence No: {doctor.licenceNo} <img src={CopyIcon} alt="" /></p>
+        </div>
+    </div>
+  )
+}
