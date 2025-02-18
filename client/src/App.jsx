4e934641d80944fad05b6140704024ddfe5e1736
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-02-18 22:05:02 +0530

    I completed 5 doctor dashboard form today including update infor forms, education from and experience form!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,42 +21,21 @@ import SupportSystem from './components/dashboard/patient/SupportSystem/SupportS
 import PrescriptionSystem from './components/dashboard/patient/PrescriptionSystem/PrescriptionSystem.jsx';
 import FAQsPage from './pages/FAQsPage/FAQsPage.jsx';
 import DoctorPublicProfilePage from './pages/DoctorProfilePage/DoctorPublicProfilePage.jsx';
-<<<<<<< HEAD
-import { SideBarStateContextProvider } from './components/contexts/SideBarStateContext.jsx';
-import UpdateInfoForm1 from './components/dashboard/doctor/forms/UpdateInfoForm/UpdateInfoForm1.jsx';
-import UpdateInfoForm2 from './components/dashboard/doctor/forms/UpdateInfoForm/UpdateInfoForm2.jsx';
-import UpdateInfoForm3 from './components/dashboard/doctor/forms/UpdateInfoForm/UpdateInfoForm3.jsx';
-=======
 import CalendarPopUp from './pages/CalendarDayPop/CalendarPopUp.jsx';
->>>>>>> fb978af6f0355dcd9f0c544992c483490adddee9
+import ExperienceForm from './components/dashboard/doctor/forms/ExperienceForm/ExperienceForm.jsx';
+import { SideBarStateContextProvider } from './components/contexts/SideBarStateContext.jsx';
+import { NotificationBarStateContextProvider } from './components/contexts/NotificationBarContext.jsx';
 
 function App() {
   return (
     <>
-
-      <Router>
-        <ScrollToTop />
+      <SideBarStateContextProvider>
+      <NotificationBarStateContextProvider>
+        <Router>
+          <ScrollToTop />
 
         <Routes>
 
-<<<<<<< HEAD
-              <Route path='/' element={<DoctorsPage />} />
-              <Route path='/support' element={<SupportPage />} />
-              <Route path='/about' element={<AboutPage />} />
-              <Route path='/doctors' element={<DoctorsPage />} />
-              <Route path='/services' element={<ServicePage />} />
-              <Route path='/news' element={<NewsSearchPage />} />
-              <Route path='/news/:id' element={<NewsPage />} />
-              <Route path='/contact' element={<ContactPage />} />
-              <Route path='/login' element={<PatientLoginPage />} />
-              <Route path='/register' element={<PatientRegistrationPage />} />
-              <Route path='/search-doctors' element={<DoctorsSearchPage />} />
-              <Route path='/test' element={<DoctorsCard filter={true} />} />
-              <Route path='/faqs' element={<FAQsPage />} />
-              <Route path='/doctor-public-profile' element={<DoctorPublicProfilePage />} />
-              <Route path='/doctor-profile/update-info' element={<UpdateInfoForm3 />} />
-            </Route>
-=======
           <Route path='/' element={<MainLayout />}>
 
             <Route path='/' element={<DoctorsPage />} />
@@ -73,8 +52,8 @@ function App() {
             <Route path='/test' element={<DoctorsCard filter={true} />} />
             <Route path='/search-faqs' element={<FAQsPage />} />
             <Route path='/doctor-profile' element={<DoctorPublicProfilePage />} />
+            <Route path='/doctor-profile/experience' element={<ExperienceForm />} />
           </Route>
->>>>>>> fb978af6f0355dcd9f0c544992c483490adddee9
 
 
 
@@ -87,8 +66,8 @@ function App() {
           </Route>
         </Routes>
       </Router>
-
-      <CalendarPopUp />
+      </NotificationBarStateContextProvider>
+      </SideBarStateContextProvider>
 
     </>
   )
