95b7bb0f5fff90d622280a9df791a8c15516ecbe
Vedant771 <vedantgupta771@gmail.com>
2025-03-11 00:04:15 +0530

    Announcement Page Done!

--- client/src/pages/AnnouncementPage/AnnouncementPage.jsx
+++ client/src/pages/AnnouncementPage/AnnouncementPage.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import './AnnouncementPage.css';
 import { useSideBarState } from '../../contexts/SideBarStateContext.jsx';
 import announcement_rectangle from '../../assets/imgs/Announcement-Page-Rectangle.png';
+import announcement_rectangle_2 from '../../assets/imgs/Pink-Rectangle-2.png';
 import announcement_speaker from '../../assets/imgs/Announcement-Page-Speaker.png';
+import announcement_2nd_speaker from '../../assets/imgs/Announcement-Page-2nd-Speaker.png';
 import arrow_down from "../../assets/icons/arrow-down.png";
 
 export default function AnnouncementPage({ }) {
@@ -62,24 +64,27 @@ export default function AnnouncementPage({ }) {
             date: "Released on 12/02/2025"
         }
     ];
+    const [moreAnnouncement,setMoreAnnouncement] = useState(false);
     return (
         <>
             <div className="AnnouncementPage">
                 <div className="Announcement-Hero-Section">
                     <div className="Announcement-Hero-Section-Content">
+                        <img src={announcement_2nd_speaker} alt="" className="nd-Speaker"/>
                         <h1>Stay Updated with the Latest Announcements</h1>
                         <p>Welcome to our announcements page, where we keep you informed about new features, important updates, and upcoming events. Stay tuned for the latest improvements designed to enhance your healthcare experience!</p>
-                        <img src={announcement_rectangle} alt="" />
+                        <img src={announcement_rectangle} alt="" className="th-Image"/>
+                        <img src={announcement_rectangle_2} alt="" className="rd-Image"/>
                     </div>
                     <div className="Announcement-Hero-Section-Image">
-                        <img src={announcement_speaker} alt="" />
+                        <img src={announcement_speaker} alt="" className="st-Speaker"/>
                     </div>
                 </div>
                 <div className="Announcement-Page-Recent-Announcements">
                     <p className="Recent-Annoucements-Heading">Recent Announcements</p>
                     <div className="Announcements-Container">
                         {
-                            recent_announcements.map((announcement, index) => (
+                            recent_announcements.slice(0,moreAnnouncement ? recent_announcements.length:2).map((announcement, index) => (
                                 <div className="Announcements-Container-Cards">
                                     <h3>{announcement.title}</h3>
                                     <p>{announcement.description}</p>
@@ -88,10 +93,10 @@ export default function AnnouncementPage({ }) {
                             ))
                         }
                     </div>
-                    <div className="View-More-Announcements">
+                    <button className="View-More-Announcements" onClick={()=>setMoreAnnouncement(!moreAnnouncement)}>
                         <p>View More Announcements</p>
                         <img src={arrow_down}/>
-                    </div>
+                    </button>
                     <p className="Recent-Annoucements-Heading">Upcoming Features</p>
                     <div className="Announcements-Container">
                         {
