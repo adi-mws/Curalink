f66d52d8790765be53d91001274834204adb1358
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-03-12 14:08:04 +0530

    Creating PatientInformation

---
+++ client/src/components/dashboard/patient/PatientInformation/PatientInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./PatientInformation.css";
+import PatientImage from "../../../../assets/imgs/PatientImage.png";
+
+const PatientInformation = () => {
+  return (
+    <div className='PatientInformation'>
+        
+    </div>
+  )
+}
+
+export default PatientInformation
