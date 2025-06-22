55e9fa8ab94aa818eb79ed7ba0ca183345c9b556
rohanraj-rr-25 <156910598+rohanraj-rr-25@users.noreply.github.com>
2025-06-22 20:35:00 +0530

    Check the about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -47,7 +47,6 @@ function App() {
       {/* <DoctorLoginPage /> */}
       {/* <DoctorRegistrationPage /> */}
       {/* <AdminLoginPage /> */}
-      <AdminSidebar />
       <AuthProvider>
         <SideBarProvider>
           <SideBarStateContextProvider>
